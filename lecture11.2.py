199148c2bac247834edab063f8017285fe133066
beginnerpyth <hskx7779@gmail.com>
2026-01-02 23:49:58 +0900

    Add files via upload
    just another day of learning

---
+++ lecture11.2.py
@@ -0,0 +1,12 @@
+input = "hello world"
+o = "#############helloworld##########"
+print(input.title)
+ 
+names = ["ham","yam","sam"]
+print(names.index('ham'))
+      
+fruits = ["aplle","banana","carrot"]
+fruits[0] = "mango"
+print(fruits)
+fruits.remove(2)
+print(fruits)
---
+++ lecture11.py
@@ -0,0 +1,22 @@
+name = "hello@world"
+name = name.split("@")#split bich ma j xa tesmai break garxa space xa vane space # xa vane #
+print(name)
+game = ('ram','hari','sam')
+b = "#".join(game)
+print(b)
+hame = "123"
+print(hame.isdigit())
+same = "hkjjjkkj"
+print(same.islower())
+bam = "hhfgfhfgffff"
+print(bam.count("h"))
+
+kame = "############ world ##############"
+b= kame.strip("#")
+c = b.replace("w","W")
+print(c)
+
+d = "fffdfdff"
+print(d.count("f"))
+
+
